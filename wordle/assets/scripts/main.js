32e77e67eca2f2766055302bd8743aebccdaf99e
CAProductions <CAProductions2021@outlook.com>
2022-05-06 08:47:28 -0400

    Update main.js

--- wordle/assets/scripts/main.js
+++ wordle/assets/scripts/main.js
@@ -1,7 +1,9 @@
 var answer = cAnswer[ Math.floor(Math.random() * cAnswer.length) ]
+var c = document.getElementById(`colors`)
+var winturn = 0
 var score = ""
   function update() {
-    document.getElementById(`write_h`).innerHTML = `Wordle Free`
+    document.getElementById(`write_h`).innerHTML = `Wordle Lite`
     console.log(`write: ${answer}`)
   }
   update()
@@ -14,19 +16,22 @@ var score = ""
 
           turn += 1
           if (answer==a){
-            
             document.getElementById(`b${turn}1`).textContent = a[0]
-            score = score+""
+            score = score + "🟩"
             document.getElementById(`b${turn}2`).textContent = a[1]
+            score = score + "🟩"
             document.getElementById(`b${turn}3`).textContent = a[2]
+            score = score + "🟩"
             document.getElementById(`b${turn}4`).textContent = a[3]
+            score = score + "🟩"
             document.getElementById(`b${turn}5`).textContent = a[4]
+            score = score + "🟩<br>"
             document.getElementById(`b${turn}1`).style.backgroundColor = ("#00c02b")
             document.getElementById(`b${turn}2`).style.backgroundColor = ("#00c02b")
             document.getElementById(`b${turn}3`).style.backgroundColor = ("#00c02b")
             document.getElementById(`b${turn}4`).style.backgroundColor = ("#00c02b")
             document.getElementById(`b${turn}5`).style.backgroundColor = ("#00c02b")
-            Toast.show(`You got it!`, `sucsess`)
+            win()
             return
           }else{
             let i = 0
@@ -35,19 +40,31 @@ var score = ""
               if (getLetter(i, a) == getLetter(i, answer)){
                 document.getElementById(`b${turn}${i}`).textContent = a[i-1]
                 document.getElementById(`b${turn}${i}`).style.backgroundColor = ("#00c02b")
+                score = score+"🟩"
+                if (i==5){
+                  score = score + "<br>"
+                }
               }else{
                 if (answer.includes(a[i-1])){
                   document.getElementById(`b${turn}${i}`).textContent = a[i-1]
                   document.getElementById(`b${turn}${i}`).style.backgroundColor = ("yellow")
+                  score = score+"🟨"
+                  if (i==5){
+                    score = score + "<br>"
+                  }
                 }else{
                   document.getElementById(`b${turn}${i}`).textContent = a[i-1]
                   document.getElementById(`b${turn}${i}`).style.backgroundColor = ("gray")
+                  score = score+"⬜"
+                  if (i==5){
+                    score = score + "<br>"
+                  }
                 }
               }
             }while(i<5)
-            if (turn==5){
-            Toast.show(`the correct word was ${answer}`, `sucsess`)
-          }
+            if (turn==6){
+            lose()
+            }
           }
         }else{
           Toast.show('That Word is Not included in word list', 'error')
@@ -58,32 +75,6 @@ var score = ""
   }
   function getLetter (n, string) {
     return `${string[n-1]}  `
-  }
-  var mode = 1
-  function toggleMode() {
-if (mode==1){
-  mode = 0
-  document.getElementById(`body`).style.backgroundColor = ("lineat")
-  document.getElementById(`body`).style.color = ("white")
-  document.getElementById(`mode_btn`).textContent = (`Light Mode`)
-  var guessboxes = document.getElementsByClassName("guess_box")
-  for (let i = 0; i < guessboxes.length; i++) {
-      const element = guessboxes[i];
-      element.style.border = "5px solid white"
-  }
-
-}else{
-  mode = 1
-  document.getElementById(`body`).style.backgroundColor = ("white")
-  document.getElementById(`body`).style.color = ("black")
-  document.getElementById(`mode_btn`).textContent = (`Dark Mode`)
-  var guessboxes = document.getElementsByClassName("guess_box")
-  for (let i = 0; i < guessboxes.length; i++) {
-      const element = guessboxes[i];
-      element.style.border = "5px solid black"
-  }
-
-}
   }
 function updateTXT(){
   var a = document.getElementById ("textarea").value
@@ -103,13 +94,17 @@ setInterval(updateTXT, 100)
 document.getElementById(`textarea`).addEventListener("input", () => {
   updateTXT()
 })
-var rdm = Math.random() * 3
-if (Math.floor(rdm)==0){
-  document.getElementById(`body`).style.backgroundImage = ("linear-gradient(90deg, blue, green)")
+  function win(){
+    score = `${turn}/6<br>${score}`
+    document.getElementById(`winscreen`).innerHTML = (`<h3>You Won!</h3><p>${score}</p><button onclick="closePopUp()">Close</button>`)
+    document.getElementById(`winscreen`).style.display = ("block")
+    document.getElementById ("textarea").disabled = "disabled"
   }
-if (Math.floor(rdm)==1){
-document.getElementById(`body`).style.backgroundImage = ("linear-gradient(90deg, rgb(2, 0, 128), rgb(255, 42, 0))")
-}
-if (Math.floor(rdm)==2){
-  document.getElementById(`body`).style.backgroundImage = ("linear-gradient(90deg, red, orange)")
+  function lose(){
+    document.getElementById(`winscreen`).innerHTML = (`<h3>You Lost!</h3><p>${score}</p><button onclick="closePopUp()">Close</button>`)
+    document.getElementById(`winscreen`).style.display = ("block")
+    document.getElementById ("textarea").disabled = "disabled"
+  }
+  function closePopUp(){
+    document.getElementById(`winscreen`).style.display = ("none")
   }
